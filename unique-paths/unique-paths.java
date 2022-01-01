389c7b49cd947ab37a54dc2e491e3009f3868e3f
alok328 <alok328raj@gmail.com>
2022-01-01 12:29:52 +0530

    Time: 0 ms (100.00%), Space: 35.4 MB (96.62%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,18 +1,11 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        return traverse(0, 0, m, n, new int[m][n]);
-    }
-    private int traverse(int i, int j, int m, int n, int[][] dp){
-        if(i>=m || j>=n){
-            return 0;
+        int totalSteps = m+n-2; 
+        int downSteps = m-1; 
+        double ans = 1;
+        for(int i=0; i<downSteps; i++){
+            ans = ans*(totalSteps-downSteps+(i+1))/(i+1);
         }
-        if(i==m-1 || j==n-1){
-            return 1;
-        }
-        if(dp[i][j]!=0){
-            return dp[i][j];
-        }
-        dp[i][j] = traverse(i+1, j, m, n, dp) + traverse(i, j+1, m, n, dp);
-        return dp[i][j];
+        return (int)ans;
     }
 }
