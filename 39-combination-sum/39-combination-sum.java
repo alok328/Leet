4d9c9cf8caf73725bd86ba35f45745145975e7d0
alok328 <alok328raj@gmail.com>
2022-01-14 16:16:16 +0530

    Time: 5 ms (48.41%), Space: 41.6 MB (20.33%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,22 +1,22 @@
 class Solution {
-    private void solve(int idx, int tsum, int[] arr, List<List<Integer>> ans, List<Integer> cur){
-    if(idx==arr.length){
-      if(tsum==0){
-        ans.add(new ArrayList<>(cur));
-      }
-      return;
-    }
-    if(arr[idx]<=tsum) {
-      cur.add(arr[idx]);
-      solve(idx, tsum - arr[idx], arr, ans, cur);
-      cur.remove(cur.size() - 1);
-    }
-    solve(idx+1, tsum, arr, ans, cur);
-  }
-    
-    public List<List<Integer>> combinationSum(int[] arr, int target) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        solve(0, target, arr, ans, new ArrayList<>());
+        solve(0, target, candidates, ans, new ArrayList<>());
         return ans;
+    }
+    
+    private void solve(int idx, int tar, int[] nums, List<List<Integer>> ans, List<Integer> cur){
+        if(idx==nums.length){
+            if(tar==0){
+                ans.add(new ArrayList<>(cur));
+            }
+            return;
+        }
+        if(nums[idx]<=tar){
+            cur.add(nums[idx]);
+            solve(idx, tar-nums[idx], nums, ans, cur);
+            cur.remove(cur.size()-1);
+        }
+        solve(idx+1, tar, nums, ans, cur);
     }
 }
