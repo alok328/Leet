786471ec1f7440019c4b0c846d64ea0aac89b6e9
alok328 <alok328raj@gmail.com>
2022-01-24 14:39:19 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (97.96%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,9 +1,26 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans = nums[0];
-        for(int i=1; i<nums.length; i++){
-            ans^=nums[i];
+        int n = nums.length;
+        int lo = 0;
+        int hi = n-2;
+        while(lo<=hi){
+            int mid = (lo+hi)>>1;
+            if(mid%2==0){
+                if(nums[mid]!=nums[mid+1]){
+                    //we are in right half
+                    hi = mid-1;
+                }else{
+                    lo = mid+1;
+                }
+            }else{
+                if(nums[mid]!=nums[mid+1]){
+                    //we are in the left half
+                    lo = mid+1;
+                }else{
+                    hi = mid-1;
+                }
+            }
         }
-        return ans;
+        return nums[lo];
     }
 }
