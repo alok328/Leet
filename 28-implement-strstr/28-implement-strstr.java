81ad7497ac44c98f805b73e8f991e3b2d7214e03
alok328 <alok328raj@gmail.com>
2022-02-23 17:44:37 +0530

    Time: 6 ms (96.09%), Space: 42.2 MB (50.33%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,16 +1,40 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int window = needle.length();
-        int needleHash = getHash(needle);
-        for(int i=0; i<=haystack.length()-window; i++){
-            if(getHash(haystack.substring(i, i+window))==needleHash){
-                return i;
+        if (haystack == null || needle == null)
+            return -1;
+        if(needle.length()==0){
+            return 0;
+        }
+        int[] lps = new int[needle.length()];
+        populatePrefixIndices(lps, needle);
+
+        int i = 0;
+        int j = 0;
+        while(i<haystack.length() && j<needle.length()){
+            if(haystack.charAt(i) == needle.charAt(j)){
+                i++;
+                j++;
+            }else if(j==0){
+                i++;
+            }else{
+                j = lps[j-1];
             }
+            if(j==needle.length())
+                return i-j;
         }
         return -1;
     }
-    
-    private int getHash(String s){
-        return s.hashCode();
+    private static void populatePrefixIndices(int[] lps, String needle) {
+        int i = 0;
+        int j = 1;
+        while(j<needle.length()){
+            if(needle.charAt(i)==needle.charAt(j)){
+                lps[j++] = ++i;
+            }else if(i==0){
+                lps[j++] = 0;
+            }else{
+                i = lps[i-1];
+            }
+        }
     }
 }
