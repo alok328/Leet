f699fe9263d2bce9edc6cb7dad002dd0a1de027c
alok328 <alok328raj@gmail.com>
2022-05-31 09:35:41 -0700

    Time: 0 ms (100.00%), Space: 39.1 MB (85.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,25 +1,14 @@
 class Solution {
-    int[] memo;
     public int fib(int n) {
-        if(n==0||n==1) {
-            return n;
-        }
-        memo = new int[n+1];
-        helper(n);
-        return memo[n];
-    }
-    
-    private int helper(int n) {
-        if(n==0 || memo[n]!=0) {
-            return memo[n];
-        }
-        if(n<0){
+        if(n==0){
             return 0;
         }
-        if(n==0 || n==1) {
-            return n;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        memo[n] = helper(n-1) + helper(n-2);
-        return memo[n];
+        return dp[n];
     }
 }
